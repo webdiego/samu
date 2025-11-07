d5f1d4c98748a78d1282206a6b51166f1e5d8170
Diego Massarni <webdiegomassarini@gmail.com>
2025-11-07 12:30:29 +0100

    wip: animation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Medal, Sprout, Activity, Blocks } from "lucide-react";
+import { TextFadeIn } from "./TextFadeIn";
+import { BlurIn } from "./BlurIn";
 const highlights = [
   {
     icon: <Activity />,
@@ -38,13 +40,20 @@ export default function Hero() {
       />
 
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full flex flex-col items-center justify-center text-white max-w-lg text-center mx-auto pb-20 px-4">
-        <h1 className="text-6xl font-black uppercase">Build the strength</h1>
-        <p className="text-sm md:text-base mt-1 max-w-sm">
-          Science-based training built around your goals and your next project.
-        </p>
-        <Button size={"sm"} variant="dark" className=" mt-5 cursor-pointer">
-          <p className="text-black uppercase font-bold text-xs ">Start today</p>
-        </Button>
+        <TextFadeIn direction="down">
+          <h1 className="text-6xl font-black uppercase">Build the strength</h1>
+        </TextFadeIn>
+        <BlurIn>
+          <p className="text-sm md:text-base mt-1 max-w-sm">
+            Science-based training built around your goals and your next
+            project.
+          </p>
+          <Button size={"sm"} variant="dark" className=" mt-5 cursor-pointer">
+            <p className="text-black uppercase font-bold text-xs ">
+              Start today
+            </p>
+          </Button>
+        </BlurIn>
       </div>
       <div className="absolute bottom-0 left-0 w-full bg-black text-white py-5 px-4">
         <div className="max-w-4xl mx-auto grid grid-cols-2 gap-5 md:grid-cols-4 items-center  w-full">
